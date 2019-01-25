0316ed1e382e27f77329b77d887ac8f5ed3dea9a
matthieu <matthieu.jasselette@gmail.com>
2019-01-25 14:38:47 +0100

    ex6_3 avec une meilleure présentation des collisions

--- ex6_3.js
+++ ex6_3.js
@@ -1,3 +1,5 @@
+const readlineSync = require("readline-sync");
+
 class Rectangle {
   constructor(topLeftXPos, topLeftYPos, width, length, name){
     this.topLeftXPos = topLeftXPos;
@@ -6,11 +8,11 @@ class Rectangle {
     this.length = length;
     this._name = name; //ajout d'un élément nom pour appeler les rectangles au sein d'une méthode ou d'une fonction.
   }
+
   //Ajout d'un getter sur le nom
   get name(){
     return this._name;
   }
-
   //méthode de collision avec un input inspirée du code touvé dans la doc mdn
   collides(otherRectangle){
     if(this.topLeftXPos < otherRectangle.topLeftXPos + otherRectangle.width &&
@@ -20,34 +22,43 @@ class Rectangle {
       return true;
     }
   }
+  //essai pour bypass la méthode toString par défaut ; raté
+  rectToString (){
+      var ret = `${this.name} : [${this.topLeftXPos} - ${this.topLeftYPos}] Longueur : ${this.length}, Largeur : ${this.width}`;
+      return ret;
+    }
 }
 
-let crazyRectangle = (rectAmount) => {
+let crazyRectangle = () => {
+  let rectAmount = readlineSync.question("Combien de rectangles voulez vous générer ?")
   //Générer un nombre donné (rectAmount) de rectangles ; Fonctionne !
+  let rectangleBox = [];
   for(i = 0 ; i < rectAmount ; i ++){
     let randomTopXPos = Math.floor(Math.random()*100);
     let randomTopYPos = Math.floor(Math.random()*100);
-    let randomWidth = Math.floor(Math.random()*25);
-    let randomLength = Math.floor(Math.random()*25);
+    let randomWidth = Math.floor(Math.random()*2.5);
+    let randomLength = Math.floor(Math.random()*2.5);
     let randomName = "rect"+(i+1); //incrémenter sur l'attribut nom des objets créés
     let rect = new Rectangle(randomTopXPos, randomTopYPos, randomWidth, randomLength, randomName);
-    //console.log(randomName); //Fonctionne
-    //console.log(this.name); //Undefined, normal, le "this" est passé via le get name
-    console.log(rect.name); //Fonctionne, appelle la méthode get name qui inclut un this
-    console.log(rect.topLeftXPos, rect.topLeftYPos, rect.width, rect.length); //Fonctionne
-  }
-  //console.log(rect42.topLeftXPos) Ne fonctionne pas ; rect42 est la valeur de this._name pour une instance de Rectangle
-
-  //Intégrer une boucle qui va utiliser collides() ; utliser 2 boucles.
+    rectangleBox.push(rect); // Important pour stocker les objets générés sinon ils disparaissent à la fin for
+  } // Retourne un array avec un nombre rectAmount de rectangles
+  //Vérifie les collisions pour chaque rectangle présent dans rectangleBox
+  let collisionCount = 0;
   for(i = 0 ; i < rectAmount ; i++){
     for (j = 0 ; j < rectAmount ; j++){
-      recti.collides(rectj); /*recti et rectj non définis
-      => Comment aller récupérer les rectangles générés ?*/
+      if (i !== j){
+        if(rectangleBox[i].collides(rectangleBox[j]) == true){//on utilise l'index des objets stockés dans rectangleBox
+          collisionCount ++;
+        //console.log(`Collision détectée entre ${JSON.stringify(rectangleBox[i])} et ${JSON.stringify(rectangleBox[j])}`);
+        console.log(`Collision détectée entre ${rectangleBox[i].rectToString()} et ${rectangleBox[j].rectToString()}`);
+        }
+      }
     }
   }
+  console.log(collisionCount);
 }
 
-crazyRectangle(3)
+crazyRectangle()
 //console.log("Contrôle 2 : ",rectangle1);
 
 
@@ -59,4 +70,15 @@ Then it will display all colliding rectangles amongst those that were generated 
   => itérateur (for each) ? Ne fonctionne que sur des arrays, pas sur des objets.
 
   Le code doit d'abord générer 1000 rectangles et ensuite les comparer chacun à tous les autres.
+
+Réassigner la méthode toString
+  Dog.prototype.toString = function dogToString() {
+  var ret = 'Dog ' + this.name + ' is a ' + this.sex + ' ' + this.color + ' ' + this.breed;
+  return ret;
+}
+
+Dog.prototype.toString = function dogToString() {
+var ret = 'Dog ' + this.name + ' is a ' + this.sex + ' ' + this.color + ' ' + this.breed;
+return ret;
+} NB trouver comment on déclare un eméthode au sein d'une classe.
 */
